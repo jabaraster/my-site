2986488a1b8de0b16f92a3c4acad17e03cec18d1
jabaraster <jabaraster@gmail.com>
2013-04-13 17:48:56 +0900

    mainte.

--- src/main/java/WebStarter.java
+++ src/main/java/WebStarter.java
@@ -10,7 +10,6 @@ public class WebStarter {
      */
     public static void main(final String[] pArgs) {
         final ServerStarter starter = new ServerStarter();
-        starter.setWebPort(8081);
         starter.start();
     }
 }
